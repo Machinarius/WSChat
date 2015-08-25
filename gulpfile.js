68230f83ca8da70314036c11b7f07c97eb8dd246
Germán Valencia <machinarius92@gmail.com>
2015-08-25 09:39:49 -0500

    Added a gulp task to run the server tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 /// <reference path="typings/gulp/gulp.d.ts" />
 
+//require('typescript-require');
+
 var gulp = require('gulp');
 var del = require('del');
+
 var typescript = require('gulp-typescript');
+var mocha = require('gulp-mocha');
 
 gulp.task('clean', function(done) {
 	del([
@@ -20,6 +24,11 @@ gulp.task('build_server', ['clean'], function() {
 		}));
 	
 	return tsResult.js.pipe(gulp.dest('Out/Server'));
+});
+
+gulp.task('test_server', ['build_server'], function() {
+	gulp.src('Out/Server/test/**/*.js', { read: false })
+		.pipe(mocha());
 });
 
 gulp.task('default', ['build_server']);
